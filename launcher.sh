efc2de9c78aaf40b923d12f79ca8912ca34b4cf7
Adbrap <adprog33@gmail.com>
2022-12-22 09:25:49 +0100

    first commit

---
+++ launcher.sh
@@ -0,0 +1,417 @@
+clear
+
+echo "                                                                         "
+echo "                                                                         "
+echo "                                                                         "
+echo "                                     ▄▄▄      ▓█████▄  ███▄ ▄███▓ ▄▄▄     ▄▄▄█████▓"
+echo "                                    ▒████▄    ▒██▀ ██▌▓██▒▀█▀ ██▒▒████▄   ▓  ██▒ ▓▒"
+echo "                                    ▒██  ▀█▄  ░██   █▌▓██    ▓██░▒██  ▀█▄ ▒ ▓██░ ▒░"
+echo "                                    ░██▄▄▄▄██ ░▓█▄   ▌▒██    ▒██ ░██▄▄▄▄██░ ▓██▓ ░ "
+echo "                                     ▓█   ▓██▒░▒████▓ ▒██▒   ░██▒ ▓█   ▓██▒ ▒██▒ ░ "
+echo "                                     ▒▒   ▓▒█░ ▒▒▓  ▒ ░ ▒░   ░  ░ ▒▒   ▓▒█░ ▒ ░░ "
+echo "                                     ▒   ▒▒ ░ ░ ▒  ▒ ░  ░      ░  ▒   ▒▒ ░   ░  "
+echo "                                    ░   ▒    ░ ░  ░ ░      ░     ░   ▒    ░   "
+echo "                                        ░  ░   ░           ░         ░  ░   "
+echo "                                             ░     "
+
+
+
+
+
+python3 /home/mat/Bureau/putain/BTC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ETH.py &
+sleep 1
+python3 /home/mat/Bureau/putain/BNB.py &
+sleep 1
+python3 /home/mat/Bureau/putain/XRP.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ADA.py &
+sleep 1
+python3 /home/mat/Bureau/putain/SOL.py &
+sleep 1
+python3 /home/mat/Bureau/putain/DOGE.py &
+sleep 1
+python3 /home/mat/Bureau/putain/MATIC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/DOT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/DAI.py &
+sleep 1
+python3 /home/mat/Bureau/putain/SHIB.py &
+sleep 1
+python3 /home/mat/Bureau/putain/TRX.py &
+sleep 1
+python3 /home/mat/Bureau/putain/AVAX.py &
+sleep 1
+python3 /home/mat/Bureau/putain/UNI.py &
+sleep 1
+python3 /home/mat/Bureau/putain/WBTC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/LEO.py &
+sleep 1
+python3 /home/mat/Bureau/putain/LTC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ATOM.py &
+sleep 1
+python3 /home/mat/Bureau/putain/LINK.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ETC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/XLM.py &
+sleep 1
+python3 /home/mat/Bureau/putain/FTT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/NEAR.py &
+sleep 1
+python3 /home/mat/Bureau/putain/CRO.py &
+sleep 1
+python3 /home/mat/Bureau/putain/XMR.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ALGO.py &
+sleep 1
+python3 /home/mat/Bureau/putain/BCH.py &
+sleep 1
+python3 /home/mat/Bureau/putain/LUNC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/QNT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/FLOW.py &
+sleep 1
+python3 /home/mat/Bureau/putain/VET.py &
+sleep 1
+python3 /home/mat/Bureau/putain/TON.py &
+sleep 1
+python3 /home/mat/Bureau/putain/APE.py &
+sleep 1
+python3 /home/mat/Bureau/putain/FIL.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ICP.py &
+sleep 1
+python3 /home/mat/Bureau/putain/HBAR.py &
+sleep 1
+python3 /home/mat/Bureau/putain/EGLD.py &
+sleep 1
+python3 /home/mat/Bureau/putain/XTZ.py &
+sleep 1
+python3 /home/mat/Bureau/putain/MANA.py &
+sleep 1
+python3 /home/mat/Bureau/putain/SAND.py &
+sleep 1
+python3 /home/mat/Bureau/putain/CHZ.py &
+sleep 1
+python3 /home/mat/Bureau/putain/EOS.py &
+sleep 1
+python3 /home/mat/Bureau/putain/AAVE.py &
+sleep 1
+python3 /home/mat/Bureau/putain/THETA.py &
+sleep 1
+python3 /home/mat/Bureau/putain/AXS.py &
+sleep 1
+python3 /home/mat/Bureau/putain/USDP.py &
+sleep 1
+python3 /home/mat/Bureau/putain/OKB.py &
+sleep 1
+python3 /home/mat/Bureau/putain/BSV.py &
+sleep 1
+python3 /home/mat/Bureau/putain/TUSD.py &
+sleep 1
+python3 /home/mat/Bureau/putain/MKR.py &
+sleep 1
+python3 /home/mat/Bureau/putain/KCS.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ZEC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/USDD.py &
+sleep 1
+python3 /home/mat/Bureau/putain/HT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/XEC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/MIOTA.py &
+sleep 1
+python3 /home/mat/Bureau/putain/BTT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/USDN.py &
+sleep 1
+python3 /home/mat/Bureau/putain/CAKE.py &
+sleep 1
+python3 /home/mat/Bureau/putain/GRT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/NEO.py &
+sleep 1
+python3 /home/mat/Bureau/putain/HNT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/KLAY.py &
+sleep 1
+python3 /home/mat/Bureau/putain/FTM.py &
+sleep 1
+python3 /home/mat/Bureau/putain/PAXG.py &
+sleep 1
+python3 /home/mat/Bureau/putain/SNX.py &
+sleep 1
+python3 /home/mat/Bureau/putain/RUNE.py &
+sleep 1
+python3 /home/mat/Bureau/putain/NEXO.py &
+sleep 1
+python3 /home/mat/Bureau/putain/GT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/DASH.py &
+sleep 1
+python3 /home/mat/Bureau/putain/CRV.py &
+sleep 1
+python3 /home/mat/Bureau/putain/BAT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ENJ.py &
+sleep 1
+python3 /home/mat/Bureau/putain/LDO.py &
+sleep 1
+python3 /home/mat/Bureau/putain/STX.py &
+sleep 1
+python3 /home/mat/Bureau/putain/KAVA.py &
+sleep 1
+python3 /home/mat/Bureau/putain/COMP.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ZIL.py &
+sleep 1
+python3 /home/mat/Bureau/putain/FEI.py &
+sleep 1
+python3 /home/mat/Bureau/putain/RVN.py &
+sleep 1
+python3 /home/mat/Bureau/putain/MINA.py &
+sleep 1
+python3 /home/mat/Bureau/putain/WAVES.py &
+sleep 1
+python3 /home/mat/Bureau/putain/XDC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/USDTC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/GMT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/TWT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/LRC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/DCR.py &
+sleep 1
+python3 /home/mat/Bureau/putain/CVX.py &
+sleep 1
+python3 /home/mat/Bureau/putain/CSPR.py &
+sleep 1
+python3 /home/mat/Bureau/putain/XEM.py &
+sleep 1
+python3 /home/mat/Bureau/putain/KSM.py &
+sleep 1
+python3 /home/mat/Bureau/putain/CELO.py &
+sleep 1
+python3 /home/mat/Bureau/putain/BTG.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ENS.py &
+sleep 1
+python3 /home/mat/Bureau/putain/1INCH.py &
+sleep 1
+python3 /home/mat/Bureau/putain/HOT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/LUNA.py &
+sleep 1
+python3 /home/mat/Bureau/putain/RSR.py &
+sleep 1
+python3 /home/mat/Bureau/putain/BNX.py &
+sleep 1
+python3 /home/mat/Bureau/putain/GUSD.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ROSE.py &
+sleep 1
+python3 /home/mat/Bureau/putain/YFI.py &
+sleep 1
+python3 /home/mat/Bureau/putain/QTUM.py &
+sleep 1
+python3 /home/mat/Bureau/putain/AR.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ANKR.py &
+sleep 1
+python3 /home/mat/Bureau/putain/GNO.py &
+sleep 1
+python3 /home/mat/Bureau/putain/GALA.py &
+sleep 1
+python3 /home/mat/Bureau/putain/TFUEL.py &
+sleep 1
+python3 /home/mat/Bureau/putain/KDA.py &
+sleep 1
+python3 /home/mat/Bureau/putain/IOTX.py &
+sleep 1
+python3 /home/mat/Bureau/putain/GLM.py &
+sleep 1
+python3 /home/mat/Bureau/putain/BTRST.py &
+sleep 1
+python3 /home/mat/Bureau/putain/JST.py &
+sleep 1
+python3 /home/mat/Bureau/putain/CEL.py &
+sleep 1
+python3 /home/mat/Bureau/putain/POLY.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ONE.py &
+sleep 1
+python3 /home/mat/Bureau/putain/HIVE.py &
+sleep 1
+python3 /home/mat/Bureau/putain/OMG.py &
+sleep 1
+python3 /home/mat/Bureau/putain/LPT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ZRX.py &
+sleep 1
+python3 /home/mat/Bureau/putain/BAL.py &
+sleep 1
+python3 /home/mat/Bureau/putain/FLUX.py &
+sleep 1
+python3 /home/mat/Bureau/putain/IOST.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ICX.py &
+sleep 1
+python3 /home/mat/Bureau/putain/BORA.py &
+sleep 1
+python3 /home/mat/Bureau/putain/AMP.py &
+sleep 1
+python3 /home/mat/Bureau/putain/XYM.py &
+sleep 1
+python3 /home/mat/Bureau/putain/SRM.py &
+sleep 1
+python3 /home/mat/Bureau/putain/KNC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ONT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/OP.py &
+sleep 1
+python3 /home/mat/Bureau/putain/GLMR.py &
+sleep 1
+python3 /home/mat/Bureau/putain/CHSB.py &
+sleep 1
+python3 /home/mat/Bureau/putain/STORJ.py &
+sleep 1
+python3 /home/mat/Bureau/putain/WAXP.py &
+sleep 1
+python3 /home/mat/Bureau/putain/SC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/WOO.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ZEN.py &
+sleep 1
+python3 /home/mat/Bureau/putain/XCH.py &
+sleep 1
+python3 /home/mat/Bureau/putain/SXP.py &
+sleep 1
+python3 /home/mat/Bureau/putain/SUSHI.py &
+sleep 1
+python3 /home/mat/Bureau/putain/IMX.py &
+sleep 1
+python3 /home/mat/Bureau/putain/MXC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/AUDIO.py &
+sleep 1
+python3 /home/mat/Bureau/putain/NFT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/UMA.py &
+sleep 1
+python3 /home/mat/Bureau/putain/PLA.py &
+sleep 1
+python3 /home/mat/Bureau/putain/SKL.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ABBC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/SCRT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/SLP.py &
+sleep 1
+python3 /home/mat/Bureau/putain/DGB.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ELON.py &
+sleep 1
+python3 /home/mat/Bureau/putain/INJ.py &
+sleep 1
+python3 /home/mat/Bureau/putain/VGX.py &
+sleep 1
+python3 /home/mat/Bureau/putain/CKB.py &
+sleep 1
+python3 /home/mat/Bureau/putain/CVC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/LSK.py &
+sleep 1
+python3 /home/mat/Bureau/putain/VLX.py &
+sleep 1
+python3 /home/mat/Bureau/putain/PUNDIX.py &
+sleep 1
+python3 /home/mat/Bureau/putain/COTI.py &
+sleep 1
+python3 /home/mat/Bureau/putain/KEEP.py &
+sleep 1
+python3 /home/mat/Bureau/putain/DAO.py &
+sleep 1
+python3 /home/mat/Bureau/putain/REN.py &
+sleep 1
+python3 /home/mat/Bureau/putain/MED.py &
+sleep 1
+python3 /home/mat/Bureau/putain/RNDR.py &
+sleep 1
+python3 /home/mat/Bureau/putain/CEEK.py &
+sleep 1
+python3 /home/mat/Bureau/putain/REEF.py &
+sleep 1
+python3 /home/mat/Bureau/putain/EWT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/PEOPLE.py &
+sleep 1
+python3 /home/mat/Bureau/putain/BTCST.py &
+sleep 1
+python3 /home/mat/Bureau/putain/WIN.py &
+sleep 1
+python3 /home/mat/Bureau/putain/NU.py &
+sleep 1
+python3 /home/mat/Bureau/putain/CELR.py &
+sleep 1
+python3 /home/mat/Bureau/putain/SYS.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ONG.py &
+sleep 1
+python3 /home/mat/Bureau/putain/XNO.py &
+sleep 1
+python3 /home/mat/Bureau/putain/REQ.py &
+sleep 1
+python3 /home/mat/Bureau/putain/DAG.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ORBS.py &
+sleep 1
+python3 /home/mat/Bureau/putain/SPELL.py &
+sleep 1
+python3 /home/mat/Bureau/putain/RLC.py &
+sleep 1
+python3 /home/mat/Bureau/putain/OCEAN.py &
+sleep 1
+python3 /home/mat/Bureau/putain/API3.py &
+sleep 1
+python3 /home/mat/Bureau/putain/SNT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/TRIBE.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ARDR.py &
+sleep 1
+python3 /home/mat/Bureau/putain/PYR.py &
+sleep 1
+python3 /home/mat/Bureau/putain/FX.py &
+sleep 1
+python3 /home/mat/Bureau/putain/UOS.py &
+sleep 1
+python3 /home/mat/Bureau/putain/POWER.py &
+sleep 1
+python3 /home/mat/Bureau/putain/ACA.py &
+sleep 1
+python3 /home/mat/Bureau/putain/PROM.py &
+sleep 1
+python3 /home/mat/Bureau/putain/DENT.py &
+sleep 1
+python3 /home/mat/Bureau/putain/CHR.py &
+sleep 1
+python3 /home/mat/Bureau/putain/MX.py &
+sleep 1
+python3 /home/mat/Bureau/putain/%launcher%
+
+
